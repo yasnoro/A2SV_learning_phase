0ebf6d8298012e5d379658d5ec3c6fd32cba78cc
unknown <pyahdreh@gmail.com>
2023-09-21 20:39:21 +0300

    palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Enter a string:");
+string input = Console.ReadLine();
+// Remove spaces, punctuation, and convert to lowercase
+string processedInput = Regex.Replace(input, @"\s+|[^a-zA-Z0-9]+", "").ToLower();
+
+int ind = 0;
+bool c = true;
+
+
+
+int l = processedInput.Length;
+while (ind < l)
+{
+    if (processedInput[ind] != processedInput[l - ind - 1])
+    {
+        c = false;
+    }
+    ind++;
+}
+
+if (c){
+    Console.WriteLine("The input is palindrome");
+}
+else{
+    Console.WriteLine("The input is not palindrome");
+}
+
+
+
+
